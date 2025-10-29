04fcc35eabc2d6a5014a1d2c7d79a3fb3f9b78d8
JadesonBruno <jadesonbruno.a@outlook.com>
2025-10-29 14:37:20 -0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+# Base image for Airflow
+FROM astrocrpublic.azurecr.io/runtime:3.1-3
+
+# Install dbt and the Postgres adapter in a virtual environment
+RUN python -m venv dbt_venv \
+    && source dbt_venv/bin/activate \
+    && pip install --no-cache-dir \
+        dbt-postgres==1.9.0 \
+    && deactivate
---
+++ dags/dag.py
@@ -0,0 +1,33 @@
+from airflow.models import Variable
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos.profiles import PostgresUserPasswordProfileMapping
+import os
+from pendulum import datetime
+
+profile_config = ProfileConfig(
+    profile_name="jornada_dw",
+    target_name="dev",
+    profile_mapping=PostgresUserPasswordProfileMapping(
+        conn_id="docker_postgres_db",
+        profile_args={"schema": "public"},
+    ),
+)
+
+my_cosmos_dag = DbtDag(
+    project_config=ProjectConfig(
+        dbt_project_path="/usr/local/airflow/dbt/jornada_dw",
+        project_name="jornada_dw",
+    ),
+    profile_config=profile_config,
+    execution_config=ExecutionConfig(
+        dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
+    ),
+    operator_args={
+        "install_deps": True,
+    },
+    schedule="@daily",
+    start_date=datetime(2025, 10, 29),
+    catchup=False,
+    dag_id=f"dag_jornada_dw",
+    default_args={"retries": 2},
+)
